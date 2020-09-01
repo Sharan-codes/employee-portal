a7228014bea60ff562d02a4413d0982c1deb5429
Sharan <sharan.s@juegostudio.net>
2020-09-01 13:22:41 +0530

    Added loading page without cache, send object to page.

--- src/routers/managerRouter.js
+++ src/routers/managerRouter.js
@@ -2,13 +2,23 @@ const Employee = require('../models/employee');
 const express = require('express');
 const router = new express.Router();
 
-router.get('/homeManager', (req, res) => {
+router.get('/homeManager', async (req, res) => {
+  try {  
     //Return to login page on back button press if already logged out
     res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-    /*if (!req.session.user ) {
+    if (!req.session.employee ) {
       return res.redirect('login.html');
-    } */
-    return res.render('pages/Navtrial');
-  });
+    }
+    const emp = await Employee.findOne({
+      empId : req.session.employee.empId
+    });
+    console.log(emp);
+		
+    return res.render('pages/Navtrial', { emp : emp });
+  }
+  catch (e) {
+    res.status(400).send(e);
+  }
+});
 
   module.exports = router;
