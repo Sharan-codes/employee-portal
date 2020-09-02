50d81883d95cfb7bf6764a84603d2c618eff0537
Sharan <sharan.s@juegostudio.net>
2020-09-02 21:06:56 +0530

    Added routes for manager viewing applied leaves, update leave status, viewing managed employees.

--- src/routers/managerRouter.js
+++ src/routers/managerRouter.js
@@ -1,5 +1,7 @@
+require('../constants');
 const Employee = require('../models/employee');
 const express = require('express');
+const Leave = require('../models/leave');
 const router = new express.Router();
 
 //Homepage for manager
@@ -21,4 +23,59 @@ router.get('/homeManager', async (req, res) => {
   }
 });
 
-  module.exports = router;
+//To view list of leaves
+router.get('/viewAppliedLeavesManager', async (req, res) => {
+	try {
+    //Return to login page on back button press if already logged out
+    res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    if (!req.session.employee ) {
+      return res.redirect('login.html');
+    }
+
+    const leaves = await Leave.find({
+      appliedTo : req.session.employee.empId,
+      status : APPLIED
+    });
+    res.render('pages/viewAppliedLeavesManager', { leaves : leaves });
+	} catch (e) {
+		res.status(500).send();
+	}
+});
+
+//To update the status of applied leaves
+router.post('/updateLeaveStatus', async (req, res) => {
+  try {
+    const leave = await Leave.findOneAndUpdate({
+      leaveId : req.query.leaveId
+    }, { status: req.query.status });
+    if (!leave) {
+      console.log("Leave not found");
+      return res.status(404).send("Leave not found");
+    }
+    return res.status(200).send("Leave updated");	
+  }
+  catch (e) {
+    res.status(500).send();
+  }
+});
+
+//To get the list of employees
+router.get('/viewManagedEmployees', async (req, res) => {
+  try {
+
+    const employees = await Employee.find({
+      empId : { $in: req.session.employee.empManaged }
+    });
+
+    if (!employees) {
+      return res.status(404).send("Not foubd");
+    }
+
+    return res.status(200).send({ employees : employees });
+  }
+  catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+module.exports = router;
